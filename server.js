27cec16cc86a22c15953c3d7e990d9ac94991b02
kevinlanzon <kevinlanzon@gmail.com>
2018-02-19 16:47:39 +0000

    files

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.sendFile(path.join(`${__dirname}/index.html`)));
+
+app.listen(8080, () => console.log('App listening on port 8080!'));
